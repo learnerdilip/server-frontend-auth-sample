84c74e073ea05bb072610c7df6fdac6acd302124
DilipSingh <dilip@Dilip-MBA.fritz.box>
2023-11-05 11:48:26 +0100

    moved the api server part to separate directory

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 
+import 'dotenv/config';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
